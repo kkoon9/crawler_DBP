6000c940f2f4d61dd2b1efb4ba138ed5302164f0
kkoon9 <rndrnjs2003@gmail.com>
2018-11-09 17:31:13 +0900

    scrape.re.py 개선된 코드

--- urllib/scrape.re.py
+++ urllib/scrape.re.py
@@ -1,4 +1,5 @@
 import re
+
 from html import unescape
 
 with open('DOTAX_url_meta.html') as f:
@@ -7,6 +8,11 @@ with open('DOTAX_url_meta.html') as f:
 # re.findall()을 사용해 다이소와 관련된 HTML을 추출한다.
 for partial_html in re.findall(r'<td class="subject searchpreview_subject">.*?</td>', html, re.DOTALL):
     # 게시물의 url 추출
-    url = re.search(r'<a href="/_c21_/(.*?)</a>', partial_html).group(1)
-    print(url)
+    url = re.search(r'<a href="/_c21_/(.*?)/a>', partial_html).group(1)
+    title = re.search('" >(.*?)<', url).group(1)
+    url = re.search(r'(.*?)>', url).group(1)
+    url = '/_c21_/' + url
+	 
+    print('url : ',url)
+    print('title : ',title)
     print('---')
