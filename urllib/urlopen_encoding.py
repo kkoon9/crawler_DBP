b44d8b69797efa57726c50069c16260db73ceec3
kkoon9 <rndrnjs2003@gmail.com>
2018-11-09 15:10:36 +0900

    urlopen_encoding.py 주석 추가
    "HTTP 헤더 기반 인코딩 추출" 주석 추가

--- urllib/urlopen_encoding.py
+++ urllib/urlopen_encoding.py
@@ -2,6 +2,7 @@ import sys
 from urllib.request import urlopen
 f = urlopen('http://cafe984.daum.net/_c21_/cafesearch?grpid=mEr9&fldid=&pagenum=&listnum=20&item=subject&head=&query=%EB%8B%A4%EC%9D%B4%EC%86%8C&attachfile_yn=&media_info=&viewtype=all&searchPeriod=all&sorttype=0&nickname=')
 
+# HTTP 헤더를 기반으로 인코딩 방식을 추출한다. [ Default : utf-8 ]
 encoding = f.info().get_content_charset(failobj="utf-8")
 text = f.read().decode(encoding)
 
