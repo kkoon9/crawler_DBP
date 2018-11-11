e00cc1859ac62fe325dcbb8b1385cf5d0ad6b040
kkoon9 <rndrnjs2003@gmail.com>
2018-11-11 21:06:09 +0900

    Crawler 실전 [3] 함수형태로 변경 - p1.py 변경

--- Crawler/p1.py
+++ Crawler/p1.py
@@ -1,15 +1,29 @@
 import requests
 import lxml.html
 
-response = requests.get('http://cafe984.daum.net/_c21_/cafesearch?grpid=mEr9&fldid=&pagenum=&listnum=20&item=subject&head=&query=%EB%8B%A4%EC%9D%B4%EC%86%8C&attachfile_yn=&media_info=&viewtype=all&searchPeriod=all&sorttype=0&nickname=')
-root = lxml.html.fromstring(response.content)
+def main():
+    """
+    크롤러의 메인 처리
+    """
+    # 여러 페이지에서 크롤링할 것이므로 Session을 사용한다.
+    session = requests.Session()
 
-# 모든 링크를 절대 URL로 변환
-root.make_links_absolute(respnse.url)
+    # scrape_list_page() : 제너레이터를 추출
+    response = session.get('http://cafe984.daum.net/_c21_/cafesearch?grpid=mEr9&fldid=&pagenum=&listnum=20&item=subject&head=&query=%EB%8B%A4%EC%9D%B4%EC%86%8C&attachfile_yn=&media_info=&viewtype=all&searchPeriod=all&sorttype=0&nickname=')
+    urls = scrape_list_page(response)
+    for url in urls:
+        print(url)
+        print('----')
+    
+def scrape_list_page(response):
+    root = lxml.html.fromstring(response.content)
+    root.make_links_absolute(response.url)
+    for a in root.cssselect('tbody > tr > td.subject.searchpreview_subject > a'):
+        if a == '#':
+            continue
+        url = a.get('href')
+        # yield 구문으로 제너레이터의 요소 반환
+        yield url
 
-for a in root.cssselect('tbody > tr > td.subject.searchpreview_subject > a'):
-    url = a.get('href')
-    print(url)
-    if url == '#':
-        continue
-    print('----')
+if __name__ == '__main__':
+    main()
