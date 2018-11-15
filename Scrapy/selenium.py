cf5648f6c9474e6b30d3014c8567d9c6a00b7533
kkoon9 <rndrnjs2003@gmail.com>
2018-11-15 10:12:51 +0900

    Ubuntu 환경에서의 에러 Windows 환경에서 해결
    login 후 다른 명령어가 먹지 않아 Windows 환경(Pycharm 사용)으로 넘어와서 코드를 실행시키니 해결되었다.

--- Scrapy/selenium.py
+++ Scrapy/selenium.py
@@ -2,52 +2,63 @@ from selenium import webdriver
 
 from bs4 import BeautifulSoup
 
-"""
+from selenium.webdriver.support.ui import WebDriverWait
 
+import time
+
+"""
 참고 사이트 : https://blog.naver.com/PostView.nhn?blogId=popqser2&logNo=221229125022&parentCategoryNo=&categoryNo=23&viewDate=&isShowPopularPosts=true&from=search 
-
 """
+
+
+# webdriver를 사용하기 위한 settings
+
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_argument("--disable-infobars")
+
 
 class DotaxCrawling():
 
     def __init__(self):
+        self.id = 'ID'  # cafe id
 
-        self.id = 'ID' # cafe id
+        self.pw = 'PW'  # cafe pw
 
-        self.pw = 'PW'   # cafe pw
+        self.delay = 3  # implicitly_wait()에 쓰이는 delay(초)
 
-        self.delay = 3         # implicily_wait()에 쓰이는 delay(초)
+        self.driver = webdriver.Chrome('C:\chromedriver')
 
-        self.keyword = "다이소" # 검색할 keyword
+        self.keyword = "다이소"  # 검색할 keyword
 
-        # driver를 사용하기 위한 settings
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument("--disable-infobars")
-        driver = webdriver.Chrome(chrome_options=chrome_options)
+        self.searchUrl = "http://cafe984.daum.net/_c21_/cafesearch?grpid=mEr9&fldid=&pagenum=&listnum=20&item=subject&head=&query=" + self.keyword + "&attachfile_yn=&media_info=&viewtype=all&searchPeriod=all&sorttype=0&nickname="
 
-        return self.login()
- 
+        self.loginUrl = 'https://logins.daum.net/accounts/loginform.do?url=http%3A%2F%2Fcafe984.daum.net%2F_c21_%2Fhome%3Fgrpid%3DmEr9&category=cafe&t__nil_navi=login'
 
     def login(self):
+        print("=============login start===========")
 
+        self.driver.get(self.loginUrl)
+        time.sleep(self.delay) # delay 초만큼 대기
 
+        self.driver.find_element_by_id('id').send_keys(self.id)
 
-        driver.get('https://logins.daum.net/accounts/loginform.do?url=http%3A%2F%2Fcafe984.daum.net%2F_c21_%2Fhome%3Fgrpid%3DmEr9&category=cafe&t__nil_navi=login')
+        self.driver.find_element_by_id('inputPwd').send_keys(self.pw)
 
-        
+        self.driver.find_element_by_id('inputPwd').submit()
 
-        driver.implicitly_wait(self.delay)
+        time.sleep(self.delay)
+        return self.search()
 
- 
+    def search(self):
+        print("=============search start===========")
+        self.driver.get(self.searchUrl)
+        time.sleep(self.delay)
 
-        driver.find_element_by_name('id').send_keys(self.id)
+        html = self.driver.page_source
 
-        driver.find_element_by_name('pw').send_keys(self.pw)
+        soup = BeautifulSoup(html, 'html.parser')
 
-        driver.find_element_by_xpath('.//*[@id="loginBtn"]').click()
-
- 
+        print("=============search finish===========")
 
 if __name__ == '__main__':
-
-    DotaxCrawling().init()
+    DotaxCrawling().login()
